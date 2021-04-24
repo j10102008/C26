83e4df147b2d6b3ae5cc1a7270cb68e5acd5437d
unknown <jasmine.4270@thegurukul.net>
2021-04-24 11:29:36 +0530

    added text

--- sketch.js
+++ sketch.js
@@ -41,6 +41,7 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
+    text ("Angry bird game",60,60)
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
